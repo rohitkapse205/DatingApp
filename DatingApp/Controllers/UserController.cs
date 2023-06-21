249833244c44c30cb7403608ef50ca60eb110967
rohit.kapse <rohit.kapse@ECO-BN8KB63>
2023-06-21 15:00:28 +0530

    Add project files.

---
+++ DatingApp/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using DatingApp.Data;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace DatingApp.Controllers
+{
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+
+        public UserController(DataContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUsers>>> GetUsers() 
+        {
+            var users =await _context.Users.ToListAsync();
+            return users;
+        }
+        [HttpGet("id")]
+
+        public async Task<ActionResult<AppUsers>> GetId(int id) {
+
+            var user =await _context.Users.FindAsync(id);
+            return user;
+
+
+
+            //  Alernate approach
+            //return await _context.Users.FindAsync(id);
+        }
+    }
+}
---
+++ DatingApp/Entities/AppUsers.cs
@@ -0,0 +1,8 @@
+﻿namespace DatingApp.Entities
+{
+    public class AppUsers
+    {
+        public int Id { get; set; }
+        public string? UserName { get; set; }
+    }
+}
